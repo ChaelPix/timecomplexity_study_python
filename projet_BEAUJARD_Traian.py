36bfadff1335b146ef0ebc2e721a70dba42e4e3b
ChaelPix <chael.pixel@gmail.com>
2024-10-09 14:01:18 +0200

    checkpoint

--- projet_BEAUJARD_Traian.py
+++ projet_BEAUJARD_Traian.py
@@ -1,3 +1,4 @@
+############################################## algos.py
 import copy
 import random
 import time
@@ -134,7 +135,7 @@ class MergeSort(SortAlgorithm):
         total_operations = left_operations + right_operations + merge_operations
         return merged_data, total_operations
 
-
+############################################## benchmark.py
 import copy
 import os
 import random
@@ -323,129 +324,132 @@ class SortBenchmark:
 
 
 
-
+############################################## menu.py
 from colorama import init, Fore, Style
 
 init(autoreset=True)
 
-def afficher_menu_titre(titre):
-    print(Fore.CYAN + Style.BRIGHT + "\n" + "="*50)
-    print(Fore.CYAN + Style.BRIGHT + f"{titre.center(50)}")
-    print(Fore.CYAN + Style.BRIGHT + "="*50 + "\n")
+class Menu:
+    def __init__(self):
+        self.algorithmes_selectionnes = []
+        self.nlist = 20
+        self.nval = 100
+        self.types_selectionnes = ['random', 'sorted', 'inverted']
+        self.n_runs = 1
+        self.benchmark_img_path = 'mon_benchmark'
 
-def choisir_algorithmes():
-    afficher_menu_titre("Sélection des Algorithmes")
+    def afficher_menu_titre(self, titre):
+        print(Fore.CYAN + Style.BRIGHT + "\n" + "="*50)
+        print(Fore.CYAN + Style.BRIGHT + f"{titre.center(50)}")
+        print(Fore.CYAN + Style.BRIGHT + "="*50 + "\n")
 
-    algos_disponibles = [
-        ('1', 'Bubble Sort', BubbleSort()),
-        ('2', 'Insertion Sort', InsertionSort()),
-        ('3', 'Selection Sort', SelectionSort()),
-        ('4', 'Heap Sort', HeapSort()),
-        ('5', 'Quick Sort', QuickSort()),
-        ('6', 'Merge Sort', MergeSort())
-    ]
-    
-    for code, name, _ in algos_disponibles:
-        print(f"{Fore.GREEN}{code}: {name}")
+    def choisir_algorithmes(self):
+        self.afficher_menu_titre("Sélection des Algorithmes")
 
-    choix = input(Fore.YELLOW + "\nEntrez les numéros des algorithmes séparés par des virgules (ex: 1,2,3) : ")
-    choix_split = choix.split(',')
+        algos_disponibles = [
+            ('1', 'Bubble Sort', BubbleSort()),
+            ('2', 'Insertion Sort', InsertionSort()),
+            ('3', 'Selection Sort', SelectionSort()),
+            ('4', 'Heap Sort', HeapSort()),
+            ('5', 'Quick Sort', QuickSort()),
+            ('6', 'Merge Sort', MergeSort())
+        ]
+        
+        for code, name, _ in algos_disponibles:
+            print(f"{Fore.GREEN}{code}: {name}")
 
-    algorithmes_selectionnes = []
-    for code, name, algo in algos_disponibles:
-        if code in choix_split:
-            algorithmes_selectionnes.append(algo)
-    
-    if not algorithmes_selectionnes:
-        print(Fore.RED + "\nAucun algorithme sélectionné. Sélection par défaut : Bubble Sort.")
-        algorithmes_selectionnes.append(BubbleSort())
-    
-    return algorithmes_selectionnes
+        choix = input(Fore.YELLOW + "\nEntrez les numéros des algorithmes séparés par des virgules (ex: 1,2,3) : ")
+        choix_split = choix.split(',')
 
-def choisir_taille_tableaux():
-    afficher_menu_titre("Paramètres des Tableaux")
+        self.algorithmes_selectionnes = [algo for code, name, algo in algos_disponibles if code in choix_split]
 
-    nlist = input(Fore.YELLOW + "Nombre de tableaux à générer (défaut 20) : ")
-    nval = input(Fore.YELLOW + "Taille initiale des tableaux (défaut 100) : ")
+        if not self.algorithmes_selectionnes:
+            print(Fore.RED + "\nAucun algorithme sélectionné. Sélection par défaut : Bubble Sort.")
+            self.algorithmes_selectionnes.append(BubbleSort())
 
-    try:
-        nlist = int(nlist)
-    except ValueError:
-        nlist = 20
+    def choisir_taille_tableaux(self):
+        self.afficher_menu_titre("Paramètres des Tableaux")
 
-    try:
-        nval = int(nval)
-    except ValueError:
-        nval = 100
+        nlist = input(Fore.YELLOW + "Nombre de tableaux à générer (défaut 20) : ")
+        nval = input(Fore.YELLOW + "Taille initiale des tableaux (défaut 100) : ")
 
-    print(Fore.GREEN + f"\nVous avez sélectionné {nlist} tableaux avec une taille initiale de {nval}.\n")
-    return nlist, nval
+        try:
+            self.nlist = int(nlist)
+        except ValueError:
+            self.nlist = 20
 
-def choisir_types_tri():
-    afficher_menu_titre("Types de Tableaux à Trier")
+        try:
+            self.nval = int(nval)
+        except ValueError:
+            self.nval = 100
 
-    print("1: Tableaux aléatoires (random)")
-    print("2: Tableaux triés (sorted)")
-    print("3: Tableaux triés à l'envers (inverted)")
+        print(Fore.GREEN + f"\nVous avez sélectionné {self.nlist} tableaux avec une taille initiale de {self.nval}.\n")
 
-    choix = input(Fore.YELLOW + "\nEntrez les numéros des types à trier, séparés par des virgules (ex: 1,3) (défaut: 1,2,3) : ")
-    choix_split = choix.split(',')
+    def choisir_types_tri(self):
+        self.afficher_menu_titre("Types de Tableaux à Trier")
 
-    types_possibles = {'1': 'random', '2': 'sorted', '3': 'inverted'}
-    types_selectionnes = [types_possibles[num] for num in choix_split if num in types_possibles]
+        print("1: Tableaux aléatoires (random)")
+        print("2: Tableaux triés (sorted)")
+        print("3: Tableaux triés à l'envers (inverted)")
 
-    if not types_selectionnes:
-        types_selectionnes = ['random', 'sorted', 'inverted']
+        choix = input(Fore.YELLOW + "\nEntrez les numéros des types à trier, séparés par des virgules (ex: 1,3) (défaut: 1,2,3) : ")
+        choix_split = choix.split(',')
 
-    print(Fore.GREEN + f"\nTypes de tableaux sélectionnés : {', '.join(types_selectionnes)}.\n")
-    return types_selectionnes
+        types_possibles = {'1': 'random', '2': 'sorted', '3': 'inverted'}
+        self.types_selectionnes = [types_possibles[num] for num in choix_split if num in types_possibles]
 
-def choisir_nb_repetitions():
-    afficher_menu_titre("Nombre de Répétitions")
+        if not self.types_selectionnes:
+            self.types_selectionnes = ['random', 'sorted', 'inverted']
 
-    n_runs = input(Fore.YELLOW + "Combien de répétitions pour chaque benchmark (défaut 1) : ")
+        print(Fore.GREEN + f"\nTypes de tableaux sélectionnés : {', '.join(self.types_selectionnes)}.\n")
 
-    try:
-        n_runs = int(n_runs)
-    except ValueError:
-        n_runs = 1
+    def choisir_nb_repetitions(self):
+        self.afficher_menu_titre("Nombre de Répétitions")
 
-    print(Fore.GREEN + f"\nLe nombre de répétitions sera : {n_runs}.\n")
-    return n_runs
-    
-def choisir_nom_fichier():
-    nom_fichier = input(Fore.YELLOW + "\nNom du dossier pour enregistrer les graphiques (défaut : mon_benchmark) : ")
-    if not nom_fichier:
-        nom_fichier = 'mon_benchmark'
+        n_runs = input(Fore.YELLOW + "Combien de répétitions pour chaque benchmark (défaut 1) : ")
 
-    print(Fore.GREEN + f"\nLe fichier sera sauvegardé sous : {nom_fichier}\n")
-    return nom_fichier
+        try:
+            self.n_runs = int(n_runs)
+        except ValueError:
+            self.n_runs = 1
 
-def fin_et_rejouer():
-    print(Fore.CYAN + Style.BRIGHT + "\n" + "="*50)
-    print(Fore.CYAN + Style.BRIGHT + "TEST TERMINÉ".center(50))
-    print(Fore.CYAN + Style.BRIGHT + "="*50 + "\n")
+        print(Fore.GREEN + f"\nLe nombre de répétitions sera : {self.n_runs}.\n")
 
-    choix = input(Fore.YELLOW + "Voulez-vous effectuer un autre test ? (O/N) : ").strip().lower()
-    if choix == 'o':
-        return True
-    else:
-        print(Fore.GREEN + "\nMerci d'avoir utilisé le programme. À bientôt !\n")
-        return False
+    def choisir_nom_fichier(self):
+        self.benchmark_img_path = input(Fore.YELLOW + "\nNom du dossier pour enregistrer les graphiques (défaut : mon_benchmark) : ")
+        if not self.benchmark_img_path:
+            self.benchmark_img_path = 'mon_benchmark'
+
+        print(Fore.GREEN + f"\nLe dossier sera sauvegardé sous : {self.benchmark_img_path}\n")
+
+    def fin_et_rejouer(self):
+        print(Fore.CYAN + Style.BRIGHT + "\n" + "="*50)
+        print(Fore.CYAN + Style.BRIGHT + "TEST TERMINÉ".center(50))
+        print(Fore.CYAN + Style.BRIGHT + "="*50 + "\n")
+
+        choix = input(Fore.YELLOW + "Voulez-vous effectuer un autre test ? (O/N) : ").strip().lower()
+        if choix == 'o':
+            return True
+        else:
+            print(Fore.GREEN + "\nMerci d'avoir utilisé le programme. À bientôt !\n")
+            return False
+
+    def lancer(self):
+        continuer = True
+        while continuer:
+            self.choisir_algorithmes()
+            self.choisir_taille_tableaux()
+            self.choisir_types_tri()
+            self.choisir_nb_repetitions()
+            self.choisir_nom_fichier()
+
+            benchmark = SortBenchmark(self.algorithmes_selectionnes, nlist=self.nlist, nval=self.nval, 
+                                      types_to_sort=self.types_selectionnes, n_runs=self.n_runs, 
+                                      savepath=self.benchmark_img_path)
+            benchmark.run()
+
+            continuer = self.fin_et_rejouer()
 
 if __name__ == "__main__":
-    continuer = True
-
-    while continuer:
-        algorithms = choisir_algorithmes()
-
-        nlist, nval = choisir_taille_tableaux()
-
-        types_to_sort = choisir_types_tri()
-        n_runs = choisir_nb_repetitions()
-        benchmark_img_path = choisir_nom_fichier()
-
-        benchmark = SortBenchmark(algorithms, nlist=nlist, nval=nval, types_to_sort=types_to_sort, n_runs=n_runs, savepath=benchmark_img_path)
-        benchmark.run()
-
-        continuer = fin_et_rejouer()
+    menu = Menu()
+    menu.lancer()
